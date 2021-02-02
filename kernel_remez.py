dd0751833d0a73cd818d0df6e49cdab303aa1502
Michael Doronin <warrior2031@mail.ru>
2021-02-02 18:48:37 +0300

    wip

--- kernel_remez.py
+++ kernel_remez.py
@@ -23,11 +23,15 @@ desired = [2, 0]
 numtaps = 63
 # taps = signal.firls(numtaps, [cutoff, fs * 0.5], desired, fs=fs)
 taps = signal.remez(numtaps, bands, desired, [1, 1], fs=fs, maxiter=20000)
-taps[abs(taps) <= 1e-4] = 0.
+taps[np.abs(taps) < 1e-5] = 0.0
 w, h = signal.freqz(taps, [1], worN=2000)
 reversed = taps[::-1]
 np.set_printoptions(precision=1000, linewidth=1)
+taps_i16 = (taps * 2**15).astype('int16')
+taps_i16_reversed = taps_i16[::-1]
 print('taps', taps)
-print('polyphase', np.array2string(reversed[0::2], separator=','))
+print('integer taps', np.array2string(taps_i16))
+print('halfband', np.array2string(reversed[0::2], separator=','))
+print('halfband integers', np.array2string(taps_i16_reversed[0::2], separator=','))
 plot_response(taps, fs, w, h, "Low-pass Filter")
-plt.show()
+# plt.show()
--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,18 @@ use std::iter;
 //     let temp = _mm_add_epi16(hi_128, lo_128);
 // }
 
-fn dot_product(a: &[f64], b: &[f64]) -> f64 {
-    a.iter().zip(b).map(|(x, y)| x * y).sum()
+fn dot_product(a: &[i16], b: &[i16]) -> i32 {
+    a.iter()
+        .zip(b)
+        .map(|(&x, &y)| (x as i32) * (y as i32))
+        .sum()
 }
 
-fn polyphase_fir_filter_8000_to_16000(a: &[f64], kernel: &[f64], out: &mut [f64]) {
+fn half_band_fir_filter(a: &[i16], kernel: &[i16], out: &mut [i16]) {
     a.windows(32)
         .flat_map(|w| {
             let i = unsafe { w.get_unchecked(15) };
-            iter::once(*i).chain(iter::once(dot_product(w, kernel)))
+            iter::once(*i).chain(iter::once((dot_product(w, kernel) >> 15) as i16))
         })
         .enumerate()
         .for_each(|(i, x)| out[i] = x)
@@ -34,39 +37,10 @@ fn polyphase_fir_filter_8000_to_16000(a: &[f64], kernel: &[f64], out: &mut [f64]
 
 fn main() {
     let input = std::env::args().nth(1).unwrap();
-    let split_kernel = [
-        -0.0009302717143512643,
-        0.0014245757701187292,
-        -0.002460787836795292,
-        0.003943971607032482,
-        -0.005989992008119632,
-        0.00874187972461201,
-        -0.012372412653986187,
-        0.017111662990475538,
-        -0.023280007063154706,
-        0.031371582397608344,
-        -0.04221487621722491,
-        0.05737093606609404,
-        -0.080191329559525,
-        0.11944349110699277,
-        -0.20739640614492658,
-        0.6350028611580476,
-        0.6350028611580476,
-        -0.20739640614492658,
-        0.11944349110699277,
-        -0.080191329559525,
-        0.05737093606609404,
-        -0.04221487621722491,
-        0.031371582397608344,
-        -0.023280007063154706,
-        0.017111662990475538,
-        -0.012372412653986187,
-        0.00874187972461201,
-        -0.005989992008119632,
-        0.003943971607032482,
-        -0.002460787836795292,
-        0.0014245757701187292,
-        -0.0009302717143512643,
+    let kernel = [
+        -30, 46, -80, 129, -196, 286, -405, 560, -762, 1027, -1383, 1879, -2627, 3913, -6795,
+        20807, 20807, -6795, 3913, -2627, 1879, -1383, 1027, -762, 560, -405, 286, -196, 129, -80,
+        46, -30,
     ];
     let spec = hound::WavSpec {
         channels: 1,
@@ -77,15 +51,12 @@ fn main() {
     let mut writer = hound::WavWriter::create("out.wav", spec).unwrap();
     eprintln!("input {}", &input);
     let mut reader = hound::WavReader::open(input).unwrap();
-    let mut padding = vec![0.0; 31];
-    let samples: Result<Vec<f64>, _> = reader
-        .samples::<i16>()
-        .map(|sample| sample.map(|s| s as f64))
-        .collect();
+    let mut padding = vec![0; 31];
+    let samples: Result<Vec<i16>, _> = reader.samples::<i16>().collect();
     let samples = samples.unwrap();
     padding.extend_from_slice(&samples[..]);
-    let mut out: Vec<f64> = vec![0.0; samples.len() * 2];
-    polyphase_fir_filter_8000_to_16000(&padding[..], &split_kernel[..], &mut out[..]);
+    let mut out: Vec<i16> = vec![0; samples.len() * 2];
+    half_band_fir_filter(&padding[..], &kernel[..], &mut out[..]);
     for sample in out {
         writer.write_sample::<i16>(sample as i16).unwrap();
     }
