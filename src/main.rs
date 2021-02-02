ab66cc2893787ede8b2be886728298a86c9ea1bf
Michael Doronin <warrior2031@mail.ru>
2021-02-02 14:45:54 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use hound;
 #[cfg(target_arch = "x86_64")]
 use std::arch::x86_64::*;
+use std::iter;
 
 // #[target_feature(enable  = "avx2")]
 // unsafe fn dot_product(a: &[i16], b: &[i16]) {
@@ -18,20 +19,15 @@ use std::arch::x86_64::*;
 // }
 
 fn dot_product(a: &[f64], b: &[f64]) -> f64 {
-    // let a = a.as_ptr() as *const [f64; 8];
-    // let b = b.as_ptr() as *const [f64; 8];
-    // let a = unsafe { std::mem::transmute::<_, [f64; 8]>(a) };
-    // let b = unsafe { std::mem::transmute::<_, [f64; 8]>(b) };
     a.iter().zip(b).map(|(x, y)| x * y).sum()
 }
 
-fn polyphase_fir_filter_8000_to_16000(a: &[f64], split_kernel: &[[f64; 8]], out: &mut [f64]) {
-    // let split_kernel = split_kernel.as_ptr() as *const [f64; 16];
-    // let split_kernel = unsafe { std::mem::transmute::<_, [[f64; 8]; 2]>(split_kernel) };
-    a.windows(8)
-        .flat_map(|w|
-            split_kernel.iter().map(move |h_i| dot_product(w, h_i))
-        )
+fn polyphase_fir_filter_8000_to_16000(a: &[f64], kernel: &[f64], out: &mut [f64]) {
+    a.windows(32)
+        .flat_map(|w| {
+            let i = unsafe { w.get_unchecked(15) };
+            iter::once(*i).chain(iter::once(dot_product(w, kernel)))
+        })
         .enumerate()
         .for_each(|(i, x)| out[i] = x)
 }
@@ -39,26 +35,38 @@ fn polyphase_fir_filter_8000_to_16000(a: &[f64], split_kernel: &[[f64; 8]], out:
 fn main() {
     let input = std::env::args().nth(1).unwrap();
     let split_kernel = [
-        [
-            -0.4509748,
-            -0.11602318,
-            -0.18187559,
-            -0.42433871,
-            1.27319174,
-            0.25463298,
-            0.14139273,
-            0.09965497,
-        ],
-        [
-            0.09965497,
-            0.14139273,
-            0.25463298,
-            1.27319174,
-            -0.42433871,
-            -0.18187559,
-            -0.11602318,
-            -0.4509748,
-        ],
+        -0.0009302717143512643,
+        0.0014245757701187292,
+        -0.002460787836795292,
+        0.003943971607032482,
+        -0.005989992008119632,
+        0.00874187972461201,
+        -0.012372412653986187,
+        0.017111662990475538,
+        -0.023280007063154706,
+        0.031371582397608344,
+        -0.04221487621722491,
+        0.05737093606609404,
+        -0.080191329559525,
+        0.11944349110699277,
+        -0.20739640614492658,
+        0.6350028611580476,
+        0.6350028611580476,
+        -0.20739640614492658,
+        0.11944349110699277,
+        -0.080191329559525,
+        0.05737093606609404,
+        -0.04221487621722491,
+        0.031371582397608344,
+        -0.023280007063154706,
+        0.017111662990475538,
+        -0.012372412653986187,
+        0.00874187972461201,
+        -0.005989992008119632,
+        0.003943971607032482,
+        -0.002460787836795292,
+        0.0014245757701187292,
+        -0.0009302717143512643,
     ];
     let spec = hound::WavSpec {
         channels: 1,
@@ -69,13 +77,15 @@ fn main() {
     let mut writer = hound::WavWriter::create("out.wav", spec).unwrap();
     eprintln!("input {}", &input);
     let mut reader = hound::WavReader::open(input).unwrap();
+    let mut padding = vec![0.0; 31];
     let samples: Result<Vec<f64>, _> = reader
         .samples::<i16>()
         .map(|sample| sample.map(|s| s as f64))
         .collect();
     let samples = samples.unwrap();
+    padding.extend_from_slice(&samples[..]);
     let mut out: Vec<f64> = vec![0.0; samples.len() * 2];
-    polyphase_fir_filter_8000_to_16000(&samples[..], &split_kernel[..], &mut out[..]);
+    polyphase_fir_filter_8000_to_16000(&padding[..], &split_kernel[..], &mut out[..]);
     for sample in out {
         writer.write_sample::<i16>(sample as i16).unwrap();
     }
